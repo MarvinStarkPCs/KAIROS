adfc4e8bb44cff8ab8aafdea07098ea3f05bb452
MarvinStarkPCs <marvineduardosantosperez@gmail.com>
2025-11-07 16:50:54 -0500

    frontend init

---
+++ public/build/assets/input-error-Dismyui4.js
@@ -0,0 +1,1 @@
+import{j as n}from"./app-Cyw7e1_8.js";import{c as o}from"./index-Nib0S9l5.js";function m({message:r,className:t="",...e}){return r?n.jsx("p",{...e,className:o("text-sm text-red-600 dark:text-red-400",t),children:r}):null}export{m as I};
